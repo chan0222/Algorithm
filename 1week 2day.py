29c257a7dbc2a7187d9ff0659850ed5b380be4dd
chan0222 <gksmftor8745@naver.com>
2025-01-07 19:38:37 +0900

    Feat : 3. 7개 뽑은 리스트 합이 100이면 끝 아니면 첨부터 다시// 반복문안에 반복문문

--- 1week 2day.py
+++ 1week 2day.py
@@ -1,14 +1,19 @@
 import random
 list_nine = []
 for i in range (1,9):
-    n= random.randint(1,100)
+    n= random.randint(1,99)
     list_nine.append(n)
 print(list_nine) # 리스트의 최종상태(9개의 요소를 한번만 출력하고 싶다면,print문을 루프 바깥으로 이동해야함함)
 
 # 여기서 9개애서 7개를 뽑고 나서 그 합이 100인걸 찾아야함
 
-list_seven =[]
-for i in range (1,7):
-    x = random.randint(list_seven) # 에러요~~
-    list_seven.append(x)
+list_seven = random.sample(list_nine,7)
+
 print(list_seven)
+
+result = sum(list_seven) 
+print(result)
+if result == 100:
+    answer = result.sort
+    print(answer)
+    exit()
