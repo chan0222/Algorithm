7048d944fdd364a19cdd6d77c0ffaabda970b6ec
chan0222 <gksmftor8745@naver.com>
2025-01-07 20:32:40 +0900

    Fix: 수정 일곱 난쟁이 1차

--- 1week 2day.py
+++ 1week 2day.py
@@ -1,10 +1,12 @@
-import random
-list_seven=[]
-for i in range(1,8): # 7개의 요소를 생성 
-    n = random.randint(1,99)
-    list_seven.append(n)
-    result = sum(list_seven)
-    if result == 100:
-        list_seven.sort() # list_seven.sort는 메서드 참조일뿐 호출 되지 않음 list_seven.sort()
-        print(list_seven)
-        break
+import sys
+dwarfs = [int(input()) for _ in range(9)]
+total = sum(dwarfs)
+
+for i in range(9):
+    for j in range(i+1,9):
+        if total - dwarfs[i] - dwarfs[j] == 100:
+            result = [dwarfs[k] for k in range(9) if k != i and k !=j]
+            result.sort()
+            for dwarf in result:
+                print(dwarf)
+            sys.exit()
