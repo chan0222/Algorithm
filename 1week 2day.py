c2283f48fe71afd17b4d0e8a1dbc511b5c603602
chan0222 <gksmftor8745@naver.com>
2025-01-07 20:20:56 +0900

    Fix docs: 틀린것 주석 추가

--- 1week 2day.py
+++ 1week 2day.py
@@ -1,10 +1,10 @@
 import random
 list_seven=[]
-for i in range(1,7):
+for i in range(1,8): # 7개의 요소를 생성 
     n = random.randint(1,99)
     list_seven.append(n)
     result = sum(list_seven)
     if result == 100:
-        list_seven.sort
+        list_seven.sort() # list_seven.sort는 메서드 참조일뿐 호출 되지 않음 list_seven.sort()
         print(list_seven)
         break
