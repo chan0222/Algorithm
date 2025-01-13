5a1e0090e78d7ecd26029137b70828e218ebac37
chan0222 <gksmftor8745@naver.com>
2025-01-13 22:05:11 +0900

    docs: 주석 추가가

--- 2week_1day.py
+++ 2week_1day.py
@@ -7,16 +7,21 @@ flag=False
 for _ in range(m):
     x,y =map(int,input().split())
 
+# x 위치 먼저 찾기
     for i in range(len(arr)):
         if arr[i]==x:
+            # x가 있는 위치의 값을 tmp_x 변수에 저장해 둘
             tmp_x=arr[i]
 
+            # 이후 y의 위치 찾기
             for j in range(len(arr)):
+                #x,y가 있는 위치의 값을 서로 리버스. 이때 탬_x변수에 저장해둔 값 사용용
                 if arr[j]==y:
                     arr[i]=arr[j]
                     arr[j]=tmp_x
                     flag=True
                     break
+            # x,y의 위치 찾기가 끝났다면 루프를 종료     
             if flag:break
 
 print(arr[1])
